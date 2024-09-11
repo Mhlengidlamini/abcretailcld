3dc03a14dcec9d0a17b273f6923507b8ba606954
Mhlengidlamini <127088435+Mhlengidlamini@users.noreply.github.com>
2024-09-11 22:47:59 +0000

    Add files via upload

---
+++ ViewModel/UploadImageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ABCRetail.ViewModel
+{
+    public class UploadImageViewModel
+    {
+        public string ProductId { get; set; }
+
+        [Required]
+        [Display(Name = "Product Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
